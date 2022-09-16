807904b42cc24e18241476d07dc3d1e725ab2ea8
Jean Bezerra <jeanbezrra@gmail.com>
2022-09-16 19:24:50 -0300

    Criação do projeto: AplicacaoPoo.Estrutural.Windows

---
+++ client/AplicacaoPoo.Estrutural.Windows/Form1.cs
@@ -0,0 +1,10 @@
+namespace AplicacaoPoo.Estrutural.Windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
