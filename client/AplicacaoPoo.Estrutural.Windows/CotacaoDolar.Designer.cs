4c18098a1d7df674094b7d22d0f29510ec537cb0
Jean Bezerra <jeanbezrra@gmail.com>
2022-09-16 21:04:08 -0300

    160920222104

--- client/AplicacaoPoo.Estrutural.Windows/CotacaoDolar.Designer.cs
+++ client/AplicacaoPoo.Estrutural.Windows/CotacaoDolar.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CotacaoDolar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(393, 321);
+            this.Name = "CotacaoDolar";
             this.Text = "CotacaoDolar";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
